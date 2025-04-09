02e50a8ffb634ce0078c930c0be419445e89710d
Susu0119 <g2310077@gl.aiu.ac.jp>
2025-04-09 15:36:18 +0900

    BoardSerivce / BoardSerivceImpl 생성 완료

---
+++ SpringBoardNew6/src/main/java/com/itwillbs/service/BoardServiceImpl.java
@@ -0,0 +1,25 @@
+package com.itwillbs.service;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.persistence.BoardDAO;
+
+/**
+ * 
+ * 실제 서비스 동작이 수행되는 객체
+ *
+ */
+
+@Service
+public class BoardServiceImpl implements BoardService {
+	
+	@Inject
+	private BoardDAO bDAO;
+	
+	@Override
+	public String getServerTime() {
+		return bDAO.getServerTime();
+	}
+}
