37ef72ee4651a42868d7559b8c1044b9596b51b6
Susu0119 <g2310077@gl.aiu.ac.jp>
2025-04-15 09:20:46 +0900

    Controller 매핑된 메서드 생성 완료

--- SpringBoardNew6/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoardNew6/src/main/java/com/itwillbs/controller/BoardController.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.itwillbs.domain.BoardVO;
 import com.itwillbs.service.BoardService;
@@ -46,7 +48,7 @@ public class BoardController {
 	
 	// 게시판 글쓰기 - POST
 	@RequestMapping(value="/regist", method =RequestMethod.POST)
-	public String registPOST(BoardVO vo) throws Exception {
+	public String registPOST(BoardVO vo, RedirectAttributes rttr) throws Exception {
 		logger.info(" registPOST() 실행! ");
 		// 인코딩 데이터 처리 - 이미 web.xml 파일에 설정되어 있음! 
 		
@@ -56,9 +58,27 @@ public class BoardController {
 		// 서비스 호출 - 게시판 글쓰기 동작을 처리 (DAO 호출)
 		bService.registBoard(vo);
 		
+		// 정보 하나 전달 ! (createOK)
+		rttr.addFlashAttribute("result", "createOK");
+		// => 1회성 데이터 전달
+		
 		// 게시판 글 목록 페이지로 이동
 		// return "/board/listAll"; (X)
 		return "redirect:/board/listAll";
+	}
+	
+	// 게시판 목록 - GET
+	@RequestMapping(value="/listAll", method=RequestMethod.GET)
+	public void listAllGET(@ModelAttribute("result") String result) throws Exception {
+		logger.info(" listAllGET() 실행 ");
+		
+		// 전달정보 result 저장
+		logger.info(" result : {}", result );
+		
+		// 기존의 DB 데이터를 가져와서 화면(view)에 출력
+		
+		// 연결된 뷰페이지로 이동 (/board/listAll.jsp)
+		
 	}
 	
 }
---
+++ SpringBoardNew6/src/main/webapp/WEB-INF/views/board/listAll.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="../include/header.jsp"%>
+
+<div class ="content">
+	<h1>listAll.jsp</h1>
+	${result }
+</div>
+
+<script type="text/javascript">
+	// EL 표현식의 데이터 -> JS 에서 출력
+	var result = '${result }';
+	// alert(result);
+	
+	if(result == "createOK") {
+		alert(" 글쓰기 완료! ");
+	}
+</script>
+
+<%@ include file="../include/footer.jsp"%>
